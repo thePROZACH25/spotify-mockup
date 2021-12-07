e5ec6b11b1886642f6e181572bd2bc53aeccc510
Zach S.L <zach.s.ledford@gmail.com>
2021-12-07 12:27:52 -0500

    styling user avatar and adding color to the center header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Center from '../components/Center'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
@@ -9,9 +10,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head> */}
 
-      <main >
+      <main className='flex'>
         <Sidebar />
-        {/* Center */}
+        <Center />
+        
       </main>
       <div>
         {/* Player */}
