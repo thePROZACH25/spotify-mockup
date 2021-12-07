cd6f58ff24c8a17cfadf0f3a8c78dd6f4b2df492
Zach S.L <zach.s.ledford@gmail.com>
2021-12-07 14:48:59 -0500

    adding custom spotify hook to access spotify;

--- components/Center.js
+++ components/Center.js
@@ -24,8 +24,8 @@ function Center() {
     return (
         <div className='flex-grow'>
             
-            <header className='absolute top-5 right-8'>
-                <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+            <header className='absolute top-5 right-8 text-white'>
+                <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
                     <img 
                     className='rounded-full w-10 h-10' 
                     src={session?.user.image} 
